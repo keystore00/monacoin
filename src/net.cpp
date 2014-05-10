5fca62b830f36eec8d1f2b39b6a19ab27812c85e
keystore00 <keystore00@gmail.com>
2014-05-10 18:15:40 +0900

    Add DNSSeed.

--- src/net.cpp
+++ src/net.cpp
@@ -1192,7 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-//    {"monacoin.org", "dnsseed.monacoin.org"},
+    {"givememona.tk", "seed.givememona.tk"},
     {NULL, NULL}
 };
 
